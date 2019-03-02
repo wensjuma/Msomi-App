9cf9b2792ec4eb7facae3692be73b19a0e54eded
softMaina <allancodes@gmail.com>
2019-03-02 11:33:03 +0300

    [Starts #164349376] setup flask environment and tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
-from app.api.v1.database import db_init
+
+from instance.config import config
+
 from app.api.v1.views.user_view import user_blueprint
+
 def start_app(app_config):
     app = Flask(__name__)
-    app.config.from_object([app_config])
+
+    app.config.from_object(config[app_config])
+
     app.register_blueprint(user_blueprint)
-    db_init
+
     return app
